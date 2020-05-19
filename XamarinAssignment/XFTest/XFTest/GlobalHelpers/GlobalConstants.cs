bd4493a62dd36d96d68b0a85442704107b32216b
BharathSimhadri_1996 <bharath.s@innomick.com>
2020-05-19 20:54:12 +0530

    commited missing files

---
+++ XamarinAssignment/XFTest/XFTest/GlobalHelpers/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFTest.GlobalHelpers
+{
+    public static class GlobalConstants
+    {
+        public const string DateFormat = "yyyy MMMM";
+    }
+}
